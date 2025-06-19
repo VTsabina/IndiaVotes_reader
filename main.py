65b9bd1064ca3ee72ca0576081870762b88894da
Valeria Tsabina <vvvtsabina@gmail.com>
2025-06-19 14:07:54 +0300

    Processes are async now. Programm works faster. TODO: Interface?

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -90,47 +92,58 @@ def evaluate_years(year, states):
         if year >= 1977 and states[i] == 'Delhi':
             states[i] = 'Delhi [1977 Onwards]'
 
+def parse_state(state, year, chrome_options, service):
+    driver = None
+    try:
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        addr = f'{url}/{lok_sabha_num[year]}/{state_codes[state]}'
+        driver.get(addr)
+
+        wait = WebDriverWait(driver, 20)
+        chart_div = wait.until(
+            EC.presence_of_element_located((By.ID, 'charttable_alliance'))
+        )
+        table = chart_div.find_element(By.CLASS_NAME, 'grid')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        state_data = []
+        for row in rows:
+            cells = row.find_elements(By.TAG_NAME, 'td') or row.find_elements(By.TAG_NAME, 'th')
+            row_texts = [cell.text for cell in cells]
+            state_data.append(row_texts)
+        return state, state_data
+    except Exception as e:
+        print(f"Error processing {state}: {e}")
+        return state, []
+    finally:
+        if driver:
+            driver.quit()
+
 def main():
     year = 0
     while year < 1947 or year > 2024:
         year = int(input('Enter year: '))
     state_list = input('Enter states (sep by comma): ').split(', ')
-
+    
     chrome_options = Options()
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--headless")
-
     service = Service(executable_path=driver_path)
+
     data = {}
+
     evaluate_years(year, state_list)
-    for state in state_list:
-        try:
-            driver = webdriver.Chrome(service=service, options=chrome_options)
-            addr = f'{url}/{lok_sabha_num[year]}/{state_codes[state]}'
-            print(addr)
-            driver.get(addr)
+    start_time = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(parse_state, state, year, chrome_options, service) for state in state_list]
+        
+        for future in concurrent.futures.as_completed(futures):
+            state, state_data = future.result()
+            data[state] = state_data
 
-            wait = WebDriverWait(driver, 20)
-
-            chart_div = wait.until(
-                EC.presence_of_element_located((By.ID, 'charttable_alliance'))
-            )
-
-            table = chart_div.find_element(By.CLASS_NAME, 'grid')
-
-            rows = table.find_elements(By.TAG_NAME, 'tr')
-            print(state)
-            data[state] = []
-            for row in rows:
-                cells = row.find_elements(By.TAG_NAME, 'td') or row.find_elements(By.TAG_NAME, 'th')
-                row_texts = [cell.text for cell in cells]
-                print(row_texts)
-                data[state].append(row_texts)
-
-        finally:
-            driver.quit()
     print_tables(data)
     save_tables_to_excel(data, "election_data.xlsx")
+    end_time = time.time()
+    print(f"Время выполнения: {end_time - start_time:.2f} секунд")
 
 
 if __name__ == '__main__':
